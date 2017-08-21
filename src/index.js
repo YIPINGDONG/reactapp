bce5674e7dde51cebc651913db68bff4bce419b3
Yiping Dong <2904276563@qq.com>
2017-08-21 21:29:03 +0100

    add a video search and try to run it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import　ReactDOM from 'react-dom'
-import SearchBar from './component/search_bar'
+import React, {Component} from 'react';
+import　ReactDOM from 'react-dom';
+import SearchBar from './component/search_bar';
+import YTSerch from 'youtube-api-search';
+import VideoList from "./component/video_list";
+
 const API_KEY ='AIzaSyDTdiN_eF4ZInFvp19-DuH-KDUwZMRm8DM';
-const App= function(){
-	return (
+
+class App extends Component{
+	  constructor(props) {
+    super(props);
+
+      this.setState(videos:[])
+     YTSerch({key: API_KEY,trem:'surfboard'},(videos)=>{ this.setState({videos});
+  
+});
+    };
+     
+
+	render(){	
+		return (
 		<div>
 	   <SearchBar/>
+	   <VideoList videos= {this.state.videos}/>
 	</div>
 	);
-	
+	}
+
 }
 
 ReactDOM.render(<App />,document.querySelector('.container'));
